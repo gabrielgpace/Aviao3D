97546f6d54970d1326083ad177f82b224ef5813f
Gabriel Goncalves <118779480+gabrielgpace@users.noreply.github.com>
2024-10-30 16:19:47 -0300

    adicao do tiro da torre

--- Jogo Maverick/Assets/PROTOTYPE/Scripts/Player.cs
+++ Jogo Maverick/Assets/PROTOTYPE/Scripts/Player.cs
@@ -4,7 +4,7 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     [SerializeField] private float rotationSpeedUpDown = 45f;
-    [SerializeField] private float rotationSpeedRightLeft = 45f;
+    [SerializeField] private float rotationSpeedRightLeft = 70f;
 
     void Update()
     {
@@ -16,15 +16,25 @@ public class Player : MonoBehaviour
         transform.Translate(Vector3.right * speed * Time.deltaTime);
         //UP
         if (Input.GetKey(KeyCode.W))
-            transform.Rotate(0, 0, -rotationSpeedUpDown * Time.deltaTime);
+            transform.Rotate(0, 
+                0, 
+                -rotationSpeedUpDown * Time.deltaTime);
         //DOWN
         if (Input.GetKey(KeyCode.S))
-            transform.Rotate(0, 0, rotationSpeedUpDown * Time.deltaTime);
+            transform.Rotate(0, 
+                0, 
+                rotationSpeedUpDown * Time.deltaTime);
         //LEFT
         if (Input.GetKey(KeyCode.A))
-            transform.Rotate(0,-rotationSpeedRightLeft * Time.deltaTime, 0);
+            transform.Rotate(rotationSpeedRightLeft * Time.deltaTime,
+                0, 
+                0
+                );
         //RIGHT
         if (Input.GetKey(KeyCode.D))
-            transform.Rotate(0,rotationSpeedRightLeft * Time.deltaTime, 0);
+            transform.Rotate(-rotationSpeedRightLeft * Time.deltaTime, 
+                0, 
+                0
+                );
     }
 }
--- Jogo Maverick/Assets/PROTOTYPE/Scripts/Tower.cs
+++ Jogo Maverick/Assets/PROTOTYPE/Scripts/Tower.cs
@@ -2,16 +2,35 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    public Transform target;
-    public Transform pivot;
-    [SerializeField] float rotationSpeed = 2f;
-    void Update()
-    {
-        Vector3 direction = target.position - pivot.position;
+    [SerializeField] private Transform target;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float shootForce = 1000f;
+    [SerializeField] private float cooldownTime = 1f;
+    private float lastShootTime;
 
-        direction.y = 0;
-        
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-        pivot.rotation = Quaternion.Slerp(pivot.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+    private void Start()
+    {
+        lastShootTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+
+            if (target.position.y < 100 && Time.time >= lastShootTime + cooldownTime)
+            {
+                Shoot();
+                lastShootTime = Time.time;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        bulletRb.AddForce(transform.forward * shootForce, ForceMode.Impulse);
     }
 }
